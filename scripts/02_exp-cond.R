6fc49291a6d638153fd772e463a19d717e2f7930
Sara K Yeo <sarakyeo@gmail.com>
2025-10-22 17:17:38 -0600

    completed DVs

--- scripts/02_exp-cond.R
+++ scripts/02_exp-cond.R
@@ -1,24 +1,174 @@
 
 # Creating experimental conditions ----------------------------------------
 ## Issue: Rain, Astro
-# Astronomy experimental conditions: Q14, Q17, Q20, Q23
-pilot |> 
-        freq(`Q14_Page Submit`)
-pilot |> 
-        freq(`Q17_Page Submit`)
-pilot |> 
-        freq(`Q20_Page Submit`)
-pilot |> 
-        freq(`Q23_Page Submit`)
+## Astronomy experimental conditions: Q14, Q17, Q20, Q23 ----------------------
+pilot |> freq(`Q14_Page Submit`)
+pilot |> freq(`Q17_Page Submit`)
+pilot |> freq(`Q20_Page Submit`)
+pilot |> freq(`Q23_Page Submit`)
 
-# Rain experimental conditions: Q35, Q38, Q41, Q44
-pilot |> 
-        freq(`Q35_Page Submit`)
-pilot |> 
-        freq(`Q38_Page Submit`)
-pilot |> 
-        freq(`Q41_Page Submit`)
-pilot |> 
-        freq(`Q44_Page Submit`)
+pilot <- pilot |> 
+        mutate(astim = case_when(
+                `Q14_Page Submit` == `Q14_Page Submit` ~ "No curious, no resolution",
+                `Q17_Page Submit` == `Q17_Page Submit` ~ "No curious, resolution",
+                `Q20_Page Submit` == `Q20_Page Submit` ~ "Curious, no resolution",
+                `Q23_Page Submit` == `Q23_Page Submit` ~ "Curious, resolution",
+        )) |> 
+        mutate(astim = factor(
+                astim,
+                levels = c(
+                        "No curious, resolution",
+                        "No curious, no resolution",
+                        "Curious, resolution",
+                        "Curious, no resolution"
+                )
+        ))
+pilot |> freq(astim)
+
+#                                   Freq   % Valid   % Valid Cum.   % Total   % Total Cum.
+# ------------------------------- ------ --------- -------------- --------- --------------
+#          No curious, resolution     63     26.25          26.25     24.23          24.23
+#       No curious, no resolution     60     25.00          51.25     23.08          47.31
+#             Curious, resolution     61     25.42          76.67     23.46          70.77
+#          Curious, no resolution     56     23.33         100.00     21.54          92.31
+#                            <NA>     20                               7.69         100.00
+#                           Total    260    100.00         100.00    100.00         100.00
 
 
+## Rain experimental conditions: Q35, Q38, Q41, Q44 ---------------
+pilot |> freq(`Q35_Page Submit`)
+pilot |> freq(`Q38_Page Submit`)
+pilot |> freq(`Q41_Page Submit`)
+pilot |> freq(`Q44_Page Submit`)
+
+pilot <- pilot |> 
+        mutate(rstim = case_when(
+                `Q35_Page Submit` == `Q35_Page Submit` ~ "Curious, resolution",
+                `Q38_Page Submit` == `Q38_Page Submit` ~ "No curious, resolution",
+                `Q41_Page Submit` == `Q41_Page Submit` ~ "Curious, no resolution",
+                `Q44_Page Submit` == `Q44_Page Submit` ~ "No curious, no resolution",
+        )) |> 
+        mutate(rstim = factor(
+                rstim,
+                levels = c(
+                        "No curious, resolution",
+                        "No curious, no resolution",
+                        "Curious, resolution",
+                        "Curious, no resolution"
+                )
+        ))
+pilot |> freq(rstim)
+
+                                #  Freq   % Valid   % Valid Cum.   % Total   % Total Cum.
+# ------------------------------- ------ --------- -------------- --------- --------------
+#          No curious, resolution     60     24.90          24.90     23.08          23.08
+#       No curious, no resolution     60     24.90          49.79     23.08          46.15
+#             Curious, resolution     62     25.73          75.52     23.85          70.00
+#          Curious, no resolution     59     24.48         100.00     22.69          92.69
+#                            <NA>     19                               7.31         100.00
+#                           Total    260    100.00         100.00    100.00         100.00
+
+
+# Curious vs. no curious condition ------------------------
+## Astronomy -------------
+pilot <- pilot |> 
+        mutate(
+                acurious = case_when(
+                        astim == "Curious, no resolution" ~ "Curious",
+                        astim == "Curious, resolution" ~ "Curious",
+                        astim == "No curious, no resolution" ~ "No curious",
+                        astim == "No curious, resolution" ~ "No curious"
+                )) |> 
+        mutate(acurious = factor(
+                acurious,
+                levels = c(
+                        "No curious",
+                        "Curious"
+                )
+        ))
+pilot |> freq(acurious)
+
+#                    Freq   % Valid   % Valid Cum.   % Total   % Total Cum.
+# ---------------- ------ --------- -------------- --------- --------------
+#       No curious    123     51.25          51.25     47.31          47.31
+#          Curious    117     48.75         100.00     45.00          92.31
+#             <NA>     20                               7.69         100.00
+#            Total    260    100.00         100.00    100.00         100.00
+
+## Rain ---------------------
+pilot <- pilot |> 
+        mutate(
+                rcurious = case_when(
+                        rstim == "Curious, no resolution" ~ "Curious",
+                        rstim == "Curious, resolution" ~ "Curious",
+                        rstim == "No curious, no resolution" ~ "No curious",
+                        rstim == "No curious, resolution" ~ "No curious"
+                )) |> 
+        mutate(rcurious = factor(
+                rcurious,
+                levels = c(
+                        "No curious",
+                        "Curious"
+                )
+        ))
+pilot |> freq(rcurious)
+
+#                    Freq   % Valid   % Valid Cum.   % Total   % Total Cum.
+# ---------------- ------ --------- -------------- --------- --------------
+#       No curious    120     49.79          49.79     46.15          46.15
+#          Curious    121     50.21         100.00     46.54          92.69
+#             <NA>     19                               7.31         100.00
+#            Total    260    100.00         100.00    100.00         100.00
+
+
+# Resolution vs. no resolution condition ------------------------
+## Astronomy -------------
+pilot <- pilot |> 
+        mutate(
+                areso = case_when(
+                        astim == "Curious, no resolution" ~ "No resolution",
+                        astim == "Curious, resolution" ~ "Resolution",
+                        astim == "No curious, no resolution" ~ "No resolution",
+                        astim == "No curious, resolution" ~ "Resolution"
+                )) |> 
+        mutate(areso = factor(
+                areso,
+                levels = c(
+                        "No resolution",
+                        "Resolution"
+                )
+        ))
+pilot |> freq(areso)
+
+#                       Freq   % Valid   % Valid Cum.   % Total   % Total Cum.
+# ------------------- ------ --------- -------------- --------- --------------
+#       No resolution    116     48.33          48.33     44.62          44.62
+#          Resolution    124     51.67         100.00     47.69          92.31
+#                <NA>     20                               7.69         100.00
+#               Total    260    100.00         100.00    100.00         100.00
+
+## Rain ------------------
+## Astronomy -------------
+pilot <- pilot |> 
+        mutate(
+                rreso = case_when(
+                        rstim == "Curious, no resolution" ~ "No resolution",
+                        rstim == "Curious, resolution" ~ "Resolution",
+                        rstim == "No curious, no resolution" ~ "No resolution",
+                        rstim == "No curious, resolution" ~ "Resolution"
+                )) |> 
+        mutate(rreso = factor(
+                rreso,
+                levels = c(
+                        "No resolution",
+                        "Resolution"
+                )
+        ))
+pilot |> freq(rreso)
+
+#                       Freq   % Valid   % Valid Cum.   % Total   % Total Cum.
+# ------------------- ------ --------- -------------- --------- --------------
+#       No resolution    119     49.38          49.38     45.77          45.77
+#          Resolution    122     50.62         100.00     46.92          92.69
+#                <NA>     19                               7.31         100.00
+#               Total    260    100.00         100.00    100.00         100.00
--- scripts/03_DVs.R
+++ scripts/03_DVs.R
@@ -1,43 +1,250 @@
 
 # Situational curiosity ---------------------------------------------------
-pilot |> 
-        select(Q25_1:Q25_4) |> 
+pilot |>
+        select(Q25_1:Q25_6, Q46_1:Q46_6) |> 
         freq()
 
 pilot <- pilot |> 
-        mutate(across(c(Q25_1:Q25_4),
-                      ~case_when(. == "Not at all" ~ 1,
-                                 . == "2" ~ 2,
-                                 . == "3" ~ 3,
-                                 . == "Somewhat" ~ 4,
-                                 . == "5" ~ 5,
-                                 . == "6" ~ 6,
-                                 . == "Very much" ~ 7),
+        mutate(across(c(Q25_1:Q25_6, Q46_1:Q46_6),
+                      ~case_when(. == "Strongly disagree" ~ 1,
+                                 . == "Disagree" ~ 2,
+                                 . == "Somewhat disagree" ~ 3,
+                                 . == "Neither agree nor disagree" ~ 4,
+                                 . == "Somewhat agree" ~ 5,
+                                 . == "Agree" ~ 6,
+                                 . == "Strongly agree" ~ 7),
                       .names = "{.col}c"))
 
 pilot |> 
-        select(Q25_1c:Q25_4c) |> 
+        select(Q25_1c:Q25_6c, Q46_1c:Q46_6c) |> 
         freq()
 
+## Astronomy ----------------
 pilot |> 
-        select(Q25_1c:Q25_4c) |> 
-        alpha() # Cronbach's alpha = .95
+        select(Q25_1c, Q25_2c, Q25_3c, Q25_4c, Q25_5c, Q25_6c) |> 
+        KMO() # Overall MSA = .88
+
+pilot |> 
+        select(Q25_1c, Q25_2c, Q25_3c, Q25_4c, Q25_5c, Q25_6c) |> 
+        cortest.bartlett() # sig.
+
+pilot |> 
+        select(Q25_1c, Q25_2c, Q25_3c, Q25_4c, Q25_5c, Q25_6c) |> 
+        fa.parallel() # 1 factor, 1 component
+
+pilot |>
+        select(Q25_1c, Q25_2c, Q25_3c, Q25_4c, Q25_5c, Q25_6c) |>
+        fa(
+                .,
+                nfactors = 1,
+                fm = "pa",
+                max.iter = 100,
+                rotate = "promax"
+        ) |> 
+        fa.diagram() # item 5 loads poorly, remove from index (but see note from line 86)
+
+pilot |> 
+        select(Q25_1c, Q25_2c, Q25_3c, Q25_4c) |> 
+        alpha() # Cronbach's alpha = .92
 
 pilot <- pilot |> 
         rowwise() |> 
-        mutate(curious = mean(
+        mutate(acurious = mean(
                 c(Q25_1c, Q25_2c, Q25_3c, Q25_4c),
                 na.rm = TRUE)
         )
 
-pilot |> freq(curious)
+pilot |> freq(acurious)
 pilot |> 
-        group_by() |> 
-        summarize(mean(curious, na.rm = TRUE),
-                  sd(curious, na.rm = TRUE),
-                  n_obs = n())
-# M = 3.65, SD = 1.65
+        group_by() |>
+        descr(acurious) # M = 4.20, SD = 1.48
+
+
+## Rain ----------------
+pilot |> 
+        select(Q46_1c, Q46_2c, Q46_3c, Q46_4c, Q46_5c, Q46_6c) |> 
+        KMO() # Overall MSA = .84
 
 pilot |> 
-        ggplot(aes(x = curious)) +
-        geom_histogram(bins = 7)
+        select(Q46_1c, Q46_2c, Q46_3c, Q46_4c, Q46_5c, Q46_6c) |> 
+        cortest.bartlett() # sig.
+
+pilot |> 
+        select(Q46_1c, Q46_2c, Q46_3c, Q46_4c, Q46_5c, Q46_6c) |> 
+        fa.parallel() # 2 factors, 1 component
+
+pilot |>
+        select(Q46_1c, Q46_2c, Q46_3c, Q46_4c, Q46_5c, Q46_6c) |> 
+        fa(
+                .,
+                nfactors = 1,
+                fm = "pa",
+                max.iter = 100,
+                rotate = "promax"
+        ) |> 
+        fa.diagram() # items 5 and 6 load poorly (probably due to topic, i.e., geosmin), remove from index
+# this probably means I should remove item 5 from the acurious index as well; went back up and fixed it
+
+pilot |> 
+        select(Q46_1c, Q46_2c, Q46_3c, Q46_4c) |> 
+        alpha() # Cronbach's alpha = .93
+
+pilot <- pilot |> 
+        rowwise() |> 
+        mutate(rcurious = mean(
+                c(Q46_1c, Q46_2c, Q46_3c, Q46_4c),
+                na.rm = TRUE)
+        )
+
+pilot |> freq(rcurious)
+pilot |> 
+        group_by() |>
+        descr(rcurious) # M = 4.57, SD = 1.45
+
+
+# Information Seeking -------------------------
+pilot |> 
+        select(Q26_1:Q26_5, Q47_1:Q47_5) |> 
+        freq()
+
+pilot <- pilot |> 
+        mutate(across(c(Q26_1:Q26_5, Q47_1:Q47_5),
+                      ~case_when(. == "Extremely unlikely" ~ 1,
+                                 . == "Moderately unlikely" ~ 2,
+                                 . == "Slightly unlikely" ~ 3,
+                                 . == "Neither likely nor unlikely" ~ 4,
+                                 . == "Slightly likely" ~ 5,
+                                 . == "Moderately likely" ~ 6,
+                                 . == "Extremely likely" ~ 7),
+                      .names = "{.col}c"))
+
+pilot |> 
+        select(Q26_1c:Q26_5c, Q47_1c:Q47_5c) |> 
+        freq()
+
+
+## Astronomy ----------------------
+pilot |> 
+        select(Q26_1c:Q26_5c) |> 
+        KMO() # Overall MSA = .87
+
+pilot |> 
+        select(Q26_1c:Q26_5c) |> 
+        cortest.bartlett() # sig.
+
+pilot |> 
+        select(Q26_1c:Q26_5c) |> 
+        fa.parallel() # 1 factor, 1 component
+
+pilot |>
+        select(Q26_1c:Q26_5c) |>
+        fa(
+                .,
+                nfactors = 1,
+                fm = "pa",
+                max.iter = 100,
+                rotate = "promax"
+        ) |> 
+        fa.diagram()
+
+pilot <- pilot |> 
+        mutate(ainfoseek = mean(
+                c(Q26_1c, Q26_2c, Q26_3c, Q26_4c, Q26_5c),
+                na.rm = TRUE
+        ))
+
+pilot |> freq(ainfoseek)
+pilot |>
+        group_by() |> 
+        descr(ainfoseek) # M = 3.11, SD = 1.58
+
+
+## Rain ----------------------
+pilot |> 
+        select(Q47_1c:Q47_5c) |> 
+        KMO() # Overall MSA = .83
+
+pilot |> 
+        select(Q47_1c:Q47_5c) |> 
+        cortest.bartlett() # sig.
+
+pilot |> 
+        select(Q47_1c:Q47_5c) |> 
+        fa.parallel() # 1 factor, 1 component
+
+pilot |>
+        select(Q47_1c:Q47_5c) |>
+        fa(
+                .,
+                nfactors = 1,
+                fm = "pa",
+                max.iter = 100,
+                rotate = "promax"
+        ) |> 
+        fa.diagram()
+
+pilot <- pilot |> 
+        mutate(rinfoseek = mean(
+                c(Q47_1c, Q47_2c, Q47_3c, Q47_4c, Q47_5c),
+                na.rm = TRUE
+        ))
+
+pilot |> freq(rinfoseek)
+pilot |>
+        group_by() |> 
+        descr(rinfoseek) # M = 3.26, SD = 1.51
+
+
+# Provided closure ----------------------------
+pilot |> 
+        select(Q27_3, Q27_4, Q48_3, Q48_4) |> 
+        freq()
+
+pilot <- pilot |> 
+        mutate(across(c(Q27_3, Q27_4, Q48_3, Q48_4),
+                      ~case_when(. == "Strongly disagree" ~ 1,
+                                 . == "Disagree" ~ 2,
+                                 . == "Somewhat disagree" ~ 3,
+                                 . == "Neither agree nor disagree" ~ 4,
+                                 . == "Somewhat agree" ~ 5,
+                                 . == "Agree" ~ 6,
+                                 . == "Strongly agree" ~ 7),
+                      .names = "{.col}c"))
+
+pilot |> 
+        select(Q27_3c, Q27_4c, Q48_3c, Q48_4c) |> 
+        freq()
+
+## Astronomy --------------------------
+pilot |> 
+        select(Q27_3c, Q27_4c) |> 
+        cor_test() # Pearson's r = .74, p < .001
+
+pilot <- pilot |> 
+        mutate(
+                aclosure = mean(
+                        c(Q27_3c, Q27_4c),
+                        na.rm = TRUE
+                )
+        )
+pilot |> freq(aclosure)
+pilot |> 
+        group_by() |> 
+        descr(aclosure) # M = 2.81, SD = 1.50
+
+## Rain --------------------
+pilot |> 
+        select(Q48_3c, Q48_4c) |> 
+        cor_test() # Pearson's r = .78, p < .001
+
+pilot <- pilot |> 
+        mutate(
+                rclosure = mean(
+                        c(Q48_3c, Q48_4c),
+                        na.rm = TRUE
+                )
+        )
+pilot |> freq(rclosure)
+pilot |> 
+        group_by() |> 
+        descr(rclosure) # M = 3.33, SD = 1.69
