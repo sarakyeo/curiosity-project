952504d84c401342599ad9a5711e31a71e0bac9d
Sara K Yeo <sarakyeo@gmail.com>
2026-05-12 14:48:31 -0600

    reviewing Qualtrics data

--- scripts/02_exp-cond.R
+++ scripts/02_exp-cond.R
@@ -1,174 +1,104 @@
 
 # Creating experimental conditions ----------------------------------------
-## Issue: Rain, Astro
-## Astronomy experimental conditions: Q14, Q17, Q20, Q23 ----------------------
-pilot |> freq(`Q20_Page Submit`)
-pilot |> freq(`Q22_Page Submit`)
-pilot |> freq(`Q24_Page Submit`)
-pilot |> freq(`Q26_Page Submit`)
+## All conditions ----------------
+cdata |> 
+        select(
+                `Q20_Page Submit`,
+                `Q22_Page Submit`,
+                `Q24_Page Submit`,
+                `Q26_Page Submit`
+        ) |> 
+        freq()
 
-pilot <- pilot |> 
-        mutate(astim = case_when(
-                `Q20_Page Submit` == `Q20_Page Submit` ~ "No curious, resolution",
-                `Q22_Page Submit` == `Q22_Page Submit` ~ "No curious, no resolution",
-                `Q24_Page Submit` == `Q24_Page Submit` ~ "Curious, resolution",
-                `Q26_Page Submit` == `Q26_Page Submit` ~ "Curious, no resolution",
-        )) |> 
-        mutate(astim = factor(
-                astim,
-                levels = c(
-                        "No curious, resolution",
-                        "No curious, no resolution",
-                        "Curious, resolution",
-                        "Curious, no resolution"
-                )
-        ))
-pilot |> freq(astim)
-
-#                                   Freq   % Valid   % Valid Cum.   % Total   % Total Cum.
-# ------------------------------- ------ --------- -------------- --------- --------------
-#          No curious, resolution     63     26.25          26.25     24.23          24.23
-#       No curious, no resolution     60     25.00          51.25     23.08          47.31
-#             Curious, resolution     61     25.42          76.67     23.46          70.77
-#          Curious, no resolution     56     23.33         100.00     21.54          92.31
-#                            <NA>     20                               7.69         100.00
-#                           Total    260    100.00         100.00    100.00         100.00
-
-
-## Rain experimental conditions: Q35, Q38, Q41, Q44 ---------------
-pilot |> freq(`Q35_Page Submit`)
-pilot |> freq(`Q38_Page Submit`)
-pilot |> freq(`Q41_Page Submit`)
-pilot |> freq(`Q44_Page Submit`)
-
-pilot <- pilot |> 
-        mutate(rstim = case_when(
-                `Q35_Page Submit` == `Q35_Page Submit` ~ "Curious, resolution",
-                `Q38_Page Submit` == `Q38_Page Submit` ~ "No curious, resolution",
-                `Q41_Page Submit` == `Q41_Page Submit` ~ "Curious, no resolution",
-                `Q44_Page Submit` == `Q44_Page Submit` ~ "No curious, no resolution",
-        )) |> 
-        mutate(rstim = factor(
-                rstim,
-                levels = c(
-                        "No curious, resolution",
-                        "No curious, no resolution",
-                        "Curious, resolution",
-                        "Curious, no resolution"
-                )
-        ))
-pilot |> freq(rstim)
-
-                                #  Freq   % Valid   % Valid Cum.   % Total   % Total Cum.
-# ------------------------------- ------ --------- -------------- --------- --------------
-#          No curious, resolution     60     24.90          24.90     23.08          23.08
-#       No curious, no resolution     60     24.90          49.79     23.08          46.15
-#             Curious, resolution     62     25.73          75.52     23.85          70.00
-#          Curious, no resolution     59     24.48         100.00     22.69          92.69
-#                            <NA>     19                               7.31         100.00
-#                           Total    260    100.00         100.00    100.00         100.00
-
-
-# Curious vs. no curious condition ------------------------
-## Astronomy -------------
-pilot <- pilot |> 
+cdata <- cdata |> 
         mutate(
-                acurious = case_when(
-                        astim == "Curious, no resolution" ~ "Curious",
-                        astim == "Curious, resolution" ~ "Curious",
-                        astim == "No curious, no resolution" ~ "No curious",
-                        astim == "No curious, resolution" ~ "No curious"
+                stim = case_when(
+                        `Q20_Page Submit` == `Q20_Page Submit` ~ "No curiosity, Resolution",
+                        `Q22_Page Submit` == `Q22_Page Submit` ~ "No curiosity, No resolution",
+                        `Q24_Page Submit` == `Q24_Page Submit` ~ "Curiosity, Resolution",
+                        `Q26_Page Submit` == `Q26_Page Submit` ~ "Curiosity, No resolution",
                 )) |> 
-        mutate(acurious = factor(
-                acurious,
-                levels = c(
-                        "No curious",
-                        "Curious"
-                )
-        ))
-pilot |> freq(acurious)
-
-#                    Freq   % Valid   % Valid Cum.   % Total   % Total Cum.
-# ---------------- ------ --------- -------------- --------- --------------
-#       No curious    123     51.25          51.25     47.31          47.31
-#          Curious    117     48.75         100.00     45.00          92.31
-#             <NA>     20                               7.69         100.00
-#            Total    260    100.00         100.00    100.00         100.00
-
-## Rain ---------------------
-pilot <- pilot |> 
         mutate(
-                rcurious = case_when(
-                        rstim == "Curious, no resolution" ~ "Curious",
-                        rstim == "Curious, resolution" ~ "Curious",
-                        rstim == "No curious, no resolution" ~ "No curious",
-                        rstim == "No curious, resolution" ~ "No curious"
-                )) |> 
-        mutate(rcurious = factor(
-                rcurious,
-                levels = c(
-                        "No curious",
-                        "Curious"
-                )
-        ))
-pilot |> freq(rcurious)
-
-#                    Freq   % Valid   % Valid Cum.   % Total   % Total Cum.
-# ---------------- ------ --------- -------------- --------- --------------
-#       No curious    120     49.79          49.79     46.15          46.15
-#          Curious    121     50.21         100.00     46.54          92.69
-#             <NA>     19                               7.31         100.00
-#            Total    260    100.00         100.00    100.00         100.00
+                stim = factor(
+                        stim,
+                        levels = c(
+                                "No curiosity, Resolution",
+                                "No curiosity, No resolution",
+                                "Curiosity, Resolution",
+                                "Curiosity, No resolution"               
+                        )
+                ))
+cdata |> freq(stim)
+#                                     Freq   % Valid   % Valid Cum.   % Total   % Total Cum.
+# --------------------------------- ------ --------- -------------- --------- --------------
+#          No curiosity, Resolution    251     25.10          25.10     25.10          25.10
+#       No curiosity, No resolution    262     26.20          51.30     26.20          51.30
+#             Curiosity, Resolution    256     25.60          76.90     25.60          76.90
+#          Curiosity, No resolution    231     23.10         100.00     23.10         100.00
+#                              <NA>      0                               0.00         100.00
+#                             Total   1000    100.00         100.00    100.00         100.00
 
 
-# Resolution vs. no resolution condition ------------------------
-## Astronomy -------------
-pilot <- pilot |> 
+## Curiosity priming conditions --------------
+cdata <- cdata |> 
         mutate(
-                areso = case_when(
-                        astim == "Curious, no resolution" ~ "No resolution",
-                        astim == "Curious, resolution" ~ "Resolution",
-                        astim == "No curious, no resolution" ~ "No resolution",
-                        astim == "No curious, resolution" ~ "Resolution"
+                cstim = case_when(
+                        stim == "No curiosity, Resolution" ~ "No curiosity",
+                        stim == "No curiosity, No resolution" ~ "No curiosity",
+                        stim == "Curiosity, Resolution" ~ "Curiosity",
+                        stim == "Curiosity, No resolution" ~ "Curiosity"
                 )) |> 
-        mutate(areso = factor(
-                areso,
-                levels = c(
-                        "No resolution",
-                        "Resolution"
-                )
-        ))
-pilot |> freq(areso)
+        mutate(
+                cstim = factor(
+                        cstim,
+                        levels = c("No curiosity", "Curiosity")
+                ))
+cdata |> freq(cstim)
+#                      Freq   % Valid   % Valid Cum.   % Total   % Total Cum.
+# ------------------ ------ --------- -------------- --------- --------------
+#       No curiosity    513     51.30          51.30     51.30          51.30
+#          Curiosity    487     48.70         100.00     48.70         100.00
+#               <NA>      0                               0.00         100.00
+#              Total   1000    100.00         100.00    100.00         100.00
 
+
+## Resolution conditions ------------------
+cdata <- cdata |> 
+        mutate(
+                rstim = case_when(
+                        stim == "No curiosity, Resolution" ~ "Resolution",
+                        stim == "No curiosity, No resolution" ~ "No resolution",
+                        stim == "Curiosity, Resolution" ~ "Resolution",
+                        stim == "Curiosity, No resolution" ~ "No resolution"
+                )) |> 
+        mutate(
+                rstim = factor(
+                        rstim,
+                        levels = c("No resolution", "Resolution")
+                ))
+cdata |> freq(rstim)
 #                       Freq   % Valid   % Valid Cum.   % Total   % Total Cum.
 # ------------------- ------ --------- -------------- --------- --------------
-#       No resolution    116     48.33          48.33     44.62          44.62
-#          Resolution    124     51.67         100.00     47.69          92.31
-#                <NA>     20                               7.69         100.00
-#               Total    260    100.00         100.00    100.00         100.00
+#       No resolution    493     49.30          49.30     49.30          49.30
+#          Resolution    507     50.70         100.00     50.70         100.00
+#                <NA>      0                               0.00         100.00
+#               Total   1000    100.00         100.00    100.00         100.00
 
-## Rain ------------------
-## Astronomy -------------
-pilot <- pilot |> 
+
+# Check DV assignment ---------------------
+cdata |> freq(`2nd_order`) # 497 assigned to DV set 1; 503 assigned to DV set 2
+cdata |> freq(Q29_14) # 497 in DV 1 
+cdata |> freq(Q30_6) # 503 in DV 2
+
+cdata <- cdata |> 
         mutate(
-                rreso = case_when(
-                        rstim == "Curious, no resolution" ~ "No resolution",
-                        rstim == "Curious, resolution" ~ "Resolution",
-                        rstim == "No curious, no resolution" ~ "No resolution",
-                        rstim == "No curious, resolution" ~ "Resolution"
+                DVset = case_when(
+                        Q29_14 == Q29_14 ~ "DV set 1: Info Seeking",
+                        Q30_6 == Q30_6 ~ "DV set 2: Risks/Benefits, Support"
                 )) |> 
-        mutate(rreso = factor(
-                rreso,
-                levels = c(
-                        "No resolution",
-                        "Resolution"
-                )
-        ))
-pilot |> freq(rreso)
-
-#                       Freq   % Valid   % Valid Cum.   % Total   % Total Cum.
-# ------------------- ------ --------- -------------- --------- --------------
-#       No resolution    119     49.38          49.38     45.77          45.77
-#          Resolution    122     50.62         100.00     46.92          92.69
-#                <NA>     19                               7.31         100.00
-#               Total    260    100.00         100.00    100.00         100.00
+        mutate(
+                DVset = factor(
+                        DVset,
+                        levels = c("DV set 1: Info Seeking", "DV set 2: Risks/Benefits, Support")
+                ))
+cdata |> freq(DVset)
