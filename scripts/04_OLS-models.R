081824f25e9f3a395399479ddf2a3865394ed182
Sara K Yeo <sarakyeo@mlpc-kc007.ad.utah.edu>
2026-06-04 14:04:55 +0200

    working on figures and tables for manuscript

--- scripts/04_OLS-models.R
+++ scripts/04_OLS-models.R
@@ -66,3 +66,68 @@ interact_plot(
                 labels = c("present", "absent")
         ) +
         jtools::theme_apa(legend.use.title = TRUE)
+
+# Export regression table for Overleaf --------
+huxreg(
+        "Curiosity" = mcur,
+        "Frustation" = mfrus,
+        "Dialogue" = mdialogue,
+        number_format = 2,
+        stars = c(`***` = 0.001, `**` = 0.01, `*` = 0.05),
+        ci_level = .95,
+        align = ".",
+        statistics = c(
+                "N" = "nobs",
+                "Adj. R-squared" = "adj.r.squared",
+                "F" = "statistic",
+                "df" = "df",
+                "p" = "p.value"
+        ),
+        error_format = "({std.error})",
+        error_pos = c("same"),
+        coefs = c(
+                "(Intercept)" = "(Intercept)",
+                "Curiosity prime (present)" = "cstimCuriosity",
+                "Resolution (present)" = "rstimResolution",
+                "Frustration" = "frustration",
+                "Trait curiosity" = "dispcurious",
+                "Situational curiosity" = "curiosity",
+                "Curiosity prime (present) × Trait curiosity" = "cstimCuriosity:dispcurious",
+                "Resolution (present) × Trait curiosity" = "rstimResolution:dispcurious"
+        )
+) |>
+        set_all_padding(0) |>
+        set_label("tab:OLS-model") |>
+        set_caption(
+                "Unstandardized regression coefficients (standard errors in parentheses followed by p-values) for regression models in the path analysis predicting intentions to engage in dialogue about astrobiology and space science."
+        ) |>
+        set_font_size(11) |> 
+        print_latex() |> 
+        capture.output(file = here::here("outputs", "tab-OLS-model.tex"))
+
+# Export interaction plot for Overleaf --------
+ixn.plot <- interact_plot(
+        model = mdialogue,
+        pred = cstim,
+        modx = dispcurious,
+        interval = TRUE,
+        int.type = c("confidence"),
+        int.width = 0.95,
+        legend.main = "Dispositional curiosity"
+) +
+        scale_y_continuous(
+                name = "Dialogic intentions",
+                limits = c(1, 7),
+                expand = c(0, 0),
+                breaks = seq(1, 7, 1)
+        ) +
+        scale_x_discrete(
+                name = "Curiosity prime",
+                labels = c("present", "absent")
+        ) +
+        jtools::theme_apa(legend.use.title = TRUE)
+
+ggsave(plot = ixn.plot,
+       here::here("outputs", "fig-cstim-dispcurious-dialogue.png"),
+       width = 7,
+       height = 5)
