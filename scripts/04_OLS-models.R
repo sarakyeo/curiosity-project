2463c7328e1c5034b3bc6ce6ecafb4d49276bcd1
Sara K Yeo <sarakyeo@mlpc-kc007.ad.utah.edu>
2026-06-04 19:40:20 +0200

    working on curiosity paper

--- scripts/04_OLS-models.R
+++ scripts/04_OLS-models.R
@@ -1,14 +1,50 @@
 # OLS path models --------------------------------
 # PROCESS cannot handle sample weights
-mcur <- cdata |> 
-        filter(DVset == "DV set 1: Info Seeking") |> 
-        select(cstim, rstim, dialogue, curiosity, frustration, interest, dispcurious, wtvar) |> 
-        lm(formula = curiosity ~ cstim + rstim + frustration + dispcurious + cstim:rstim + cstim:dispcurious + rstim:dispcurious + cstim:rstim:dispcurious, weights = wtvar)
+mcur <- cdata |>
+        filter(DVset == "DV set 1: Info Seeking") |>
+        select(
+                cstim,
+                rstim,
+                dialogue,
+                curiosity,
+                frustration,
+                interest,
+                dispcurious,
+                wtvar
+        ) |>
+        lm(
+                formula = curiosity ~ cstim +
+                        rstim +
+                        frustration +
+                        dispcurious +
+                        cstim:rstim +
+                        cstim:dispcurious +
+                        rstim:dispcurious,
+                weights = wtvar
+        )
 
-mfrus <- cdata |> 
-        filter(DVset == "DV set 1: Info Seeking") |> 
-        select(cstim, rstim, dialogue, curiosity, frustration, interest, dispcurious, wtvar) |> 
-        lm(formula = frustration ~ cstim + rstim + curiosity + dispcurious + cstim:rstim + cstim:dispcurious + rstim:dispcurious + cstim:rstim:dispcurious, weights = wtvar)
+mfrus <- cdata |>
+        filter(DVset == "DV set 1: Info Seeking") |>
+        select(
+                cstim,
+                rstim,
+                dialogue,
+                curiosity,
+                frustration,
+                interest,
+                dispcurious,
+                wtvar
+        ) |>
+        lm(
+                formula = frustration ~ cstim +
+                        rstim +
+                        curiosity +
+                        dispcurious +
+                        cstim:rstim +
+                        cstim:dispcurious +
+                        rstim:dispcurious,
+                weights = wtvar
+        )
 
 mdialogue <- cdata |>
         filter(DVset == "DV set 1: Info Seeking") |>
@@ -30,15 +66,9 @@ mdialogue <- cdata |>
                         dispcurious +
                         cstim:rstim +
                         cstim:dispcurious +
-                        rstim:dispcurious +
-                        cstim:rstim:dispcurious,
+                        rstim:dispcurious,
                 weights = wtvar
         )
-
-huxreg("Curiosity" = mcur, "Frustation" = mfrus, "Dialogue" = mdialogue,
-stars = c(`***` = .001, `**` = .01, `*` = .05),
-error_format = "({std.error}) {p.value}",
-error_pos = "same")
 
 cdata |> 
         select(dispcurious, frustration, curiosity, dialogue) |> 
@@ -76,72 +106,72 @@ interact_plot(
         jtools::theme_apa(legend.use.title = TRUE)
 
 # Export regression table for Overleaf --------
-huxreg(
-        "Curiosity" = mcur,
-        "Frustation" = mfrus,
-        "Dialogue" = mdialogue,
-        number_format = 2,
-        stars = c(`***` = 0.001, `**` = 0.01, `*` = 0.05),
-        ci_level = .95,
-        align = ".",
-        statistics = c(
-                "N" = "nobs",
-                "Adj. R-squared" = "adj.r.squared",
-                "F" = "statistic",
-                "df" = "df",
-                "p" = "p.value"
-        ),
-        error_format = "({std.error})",
-        error_pos = c("same"),
-        coefs = c(
-                "(Intercept)" = "(Intercept)",
-                "Curiosity prime (present)" = "cstimCuriosity",
-                "Resolution (present)" = "rstimResolution",
-                "Frustration" = "frustration",
-                "Trait curiosity" = "dispcurious",
-                "Situational curiosity" = "curiosity",
-                "Curiosity prime (present) × Trait curiosity" = "cstimCuriosity:dispcurious",
-                "Resolution (present) × Trait curiosity" = "rstimResolution:dispcurious",
-                "Curiosity prime (present) × Resolution prime (present)" = "cstimCuriosity:rstimResolution",
-                "Curiosity prime (present) × Resolution prime (present) × Trait curiosity" = "cstimCuriosity:rstimResolution:dispcurious"
-        )
-) |>
-        set_all_padding(0) |>
-        set_label("tab:OLS-model") |>
-        set_caption(
-                "Unstandardized regression coefficients (standard errors in parentheses followed by p-values) for regression models in the path analysis predicting intentions to engage in dialogue about astrobiology and space science."
-        ) |>
-        set_font_size(11) |> 
-        print_latex() |> 
-        capture.output(file = here::here("outputs", "tab-OLS-model.tex"))
+# huxreg(
+#         "Curiosity" = mcur,
+#         "Frustation" = mfrus,
+#         "Dialogue" = mdialogue,
+#         number_format = 2,
+#         stars = c(`***` = 0.001, `**` = 0.01, `*` = 0.05),
+#         ci_level = .95,
+#         align = ".",
+#         statistics = c(
+#                 "N" = "nobs",
+#                 "Adj. R-squared" = "adj.r.squared",
+#                 "F" = "statistic",
+#                 "df" = "df",
+#                 "p" = "p.value"
+#         ),
+#         error_format = "({std.error})",
+#         error_pos = c("same"),
+#         coefs = c(
+#                 "(Intercept)" = "(Intercept)",
+#                 "Curiosity prime (present)" = "cstimCuriosity",
+#                 "Resolution (present)" = "rstimResolution",
+#                 "Frustration" = "frustration",
+#                 "Trait curiosity" = "dispcurious",
+#                 "Situational curiosity" = "curiosity",
+#                 "Curiosity prime (present) × Trait curiosity" = "cstimCuriosity:dispcurious",
+#                 "Resolution (present) × Trait curiosity" = "rstimResolution:dispcurious",
+#                 "Curiosity prime (present) × Resolution prime (present)" = "cstimCuriosity:rstimResolution",
+#                 "Curiosity prime (present) × Resolution prime (present) × Trait curiosity" = "cstimCuriosity:rstimResolution:dispcurious"
+#         )
+# ) |>
+#         set_all_padding(0) |>
+#         set_label("tab:OLS-model") |>
+#         set_caption(
+#                 "Unstandardized regression coefficients (standard errors in parentheses followed by p-values) for regression models in the path analysis predicting intentions to engage in dialogue about astrobiology and space science."
+#         ) |>
+#         set_font_size(11) |> 
+#         print_latex() |> 
+#         capture.output(file = here::here("outputs", "tab-OLS-model.tex"))
 
 # Export interaction plot for Overleaf --------
-ixn.plot <- interact_plot(
-        model = mdialogue,
-        pred = cstim,
-        modx = dispcurious,
-        mod2 = rstim,
-        mod2.labels = c("No resolution", "Resolution"),
-        interval = TRUE,
-        int.type = c("confidence"),
-        int.width = 0.95,
-        legend.main = "Trait curiosity"
-) +
-        scale_y_continuous(
-                name = "Dialogic intentions",
-                limits = c(1, 7),
-                expand = c(0, 0),
-                breaks = seq(1, 7, 1)
-        ) +
-        scale_x_discrete(
-                name = "Curiosity prime"
-        ) +
-        jtools::theme_apa(legend.use.title = TRUE)
+# ixn.plot <- interact_plot(
+#         model = mdialogue,
+#         pred = cstim,
+#         modx = dispcurious,
+#         mod2 = rstim,
+#         mod2.labels = c("No resolution", "Resolution"),
+#         interval = TRUE,
+#         int.type = c("confidence"),
+#         int.width = 0.95,
+#         legend.main = "Trait curiosity"
+# ) +
+#         scale_y_continuous(
+#                 name = "Dialogic intentions",
+#                 limits = c(1, 7),
+#                 expand = c(0, 0),
+#                 breaks = seq(1, 7, 1)
+#         ) +
+#         scale_x_discrete(
+#                 name = "Curiosity prime"
+#         ) +
+#         jtools::theme_apa(legend.use.title = TRUE)
 
-ggsave(plot = ixn.plot,
-       here::here("outputs", "fig-three-way.png"),
-       width = 10,
-       height = 5)
+# ggsave(plot = ixn.plot,
+#        here::here("outputs", "fig-three-way.png"),
+#        width = 10,
+#        height = 5)
 
 # Next step is to try the OLS regression models with Bayesian statistics
 
@@ -200,18 +230,24 @@ huxreg(
         error_pos = c("same"),
         coefs = c(
                 "(Intercept)" = "(Intercept)",
-                "Curiosity prime (present)" = "cstimCuriosity",
-                "Resolution (present)" = "rstimResolution",
+                "Curiosity manipulation (present)" = "cstimCuriosity",
+                "Resolution manipulation (present)" = "rstimResolution",
                 "Frustration" = "frustration",
                 "Trait curiosity" = "dispcurious",
                 "Situational curiosity" = "curiosity",
-                "Curiosity prime (present) × Trait curiosity" = "cstimCuriosity:dispcurious",
-                "Resolution (present) × Trait curiosity" = "rstimResolution:dispcurious",
-                "Curiosity prime (present) × Resolution (present)" = "cstimCuriosity:rstimResolution"
-        ))
+                "Trait curiosity × Curiosity manip. (present)" = "cstimCuriosity:dispcurious",
+                "Trait curiosity × Resolution manip. (present)" = "rstimResolution:dispcurious",
+                "Curiosity manip. (present) × Resolution manip. (present)" = "cstimCuriosity:rstimResolution"
+        )
+) |>
+        set_font_size(11) |>
+        set_label("tab:OLS-model") |>
+        set_caption("Unstandardized regression coefficients and standard errors (in parentheses) in the OLS regression models predicting situational curiosity, frustration, and intentions to engage in dialogue about astrobiology and space sciences.") |>
+        print_latex() |>
+        capture.output(file = here::here("outputs", "tab-OLS-model.tex"))
 
 ## Interaction plot ------------
-interact_plot(
+ixn.plot1 <- interact_plot(
         model = mcur,
         pred = cstim,
         modx = dispcurious,
@@ -231,7 +267,7 @@ interact_plot(
         ) +
         jtools::theme_apa(legend.use.title = TRUE)
 
-interact_plot(
+ixn.plot2 <- interact_plot(
         model = mdialogue,
         pred = cstim,
         modx = dispcurious,
@@ -241,7 +277,7 @@ interact_plot(
         legend.main = "Trait curiosity"
 ) +
         scale_y_continuous(
-                name = "Dialogic intentions",
+                name = "Intentions to engage in dialogue",
                 limits = c(1, 7),
                 expand = c(0, 0),
                 breaks = seq(1, 7, 1)
@@ -250,3 +286,17 @@ interact_plot(
                 name = "Curiosity prime"
         ) +
         jtools::theme_apa(legend.use.title = TRUE)
+
+ggsave(
+        ixn.plot1,
+        filename = here::here("outputs", "fig-mcur.png"),
+        width = 6.5,
+        height = 5
+)
+
+ggsave(
+        ixn.plot2,
+        filename = here::here("outputs", "fig-mdialogue.png"),
+        width = 6.5,
+        height = 5
+)
