76b08f0408672af77e9c490c4e4ae19cebc891e0
Sara K Yeo <sarakyeo@mlpc-kc007.ad.utah.edu>
2026-06-09 23:26:42 +0200

    added graphs of means to scripts

--- scripts/04_ANOVAs.R
+++ scripts/04_ANOVAs.R
@@ -6,6 +6,22 @@ cdata |>
         lm(formula = curiosity ~ cstim + rstim + dispcurious) |> 
         summ() # ns
 
+cdata |> 
+        select(cstim, rstim, dispcurious, curiosity, frustration, infoseek, dialogue) |> 
+        ggplot(aes(x = cstim, y = curiosity)) +
+        scale_y_continuous(name = "Situational curiosity",
+                           limits = c(1,7),
+                           breaks = seq(1, 7, 1), 
+                           expand = c(0,0)) + 
+        scale_x_discrete(name = "") +
+        theme_apa() +
+        stat_summary(fun = mean, geom = "point") +
+        stat_summary(fun.data = mean_cl_normal, geom = "errorbar", width = 0.2) +
+        theme(title = element_text(size = 12, face = "bold"),
+              axis.title.x = element_text(size = 12, face = "plain"),
+              axis.title.y = element_text(size = 12, face = "plain"),
+              axis.text = element_text(size = 12, face = "plain"))
+
 # Frustration --------------------
 cdata |> 
         select(cstim, rstim, dispcurious, curiosity, frustration, dialogue) |> 
@@ -13,10 +29,66 @@ cdata |>
         lm(formula = frustration ~ cstim + rstim + dispcurious) |> 
         summ() # ns
 
+cdata |> 
+        select(cstim, rstim, dispcurious, curiosity, frustration, infoseek, dialogue) |> 
+        ggplot(aes(x = rstim, y = frustration)) +
+        scale_y_continuous(name = "Experienced frustration",
+                           limits = c(1,7),
+                           breaks = seq(1, 7, 1), 
+                           expand = c(0,0)) + 
+        scale_x_discrete(name = "") +
+        theme_apa() +
+        stat_summary(fun = mean, geom = "point") +
+        stat_summary(fun.data = mean_cl_normal, geom = "errorbar", width = 0.2) +
+        theme(title = element_text(size = 12, face = "bold"),
+              axis.title.x = element_text(size = 12, face = "plain"),
+              axis.title.y = element_text(size = 12, face = "plain"),
+              axis.text = element_text(size = 12, face = "plain"))
+
 # Intentions to engage in dialogue --------------------
 cdata |> 
-        select(cstim, rstim, dispcurious, curiosity, frustration, dialogue) |> 
+        select(cstim, rstim, dispcurious, curiosity, frustration, infoseek, dialogue) |> 
         na.omit() |> 
         lm(formula = dialogue ~ cstim + rstim + dispcurious) |> 
         summ() # ns
+
+cdata |> 
+        select(cstim, rstim, dispcurious, curiosity, frustration, infoseek, dialogue) |> 
+        ggplot(aes(x = cstim, y = dialogue)) +
+        scale_y_continuous(name = "Intentions to engage in dialogue",
+                           limits = c(1,7),
+                           breaks = seq(1, 7, 1), 
+                           expand = c(0,0)) + 
+        scale_x_discrete(name = "") +
+        theme_apa() +
+        stat_summary(fun = mean, geom = "point") +
+        stat_summary(fun.data = mean_cl_normal, geom = "errorbar", width = 0.2) +
+        theme(title = element_text(size = 12, face = "bold"),
+              axis.title.x = element_text(size = 12, face = "plain"),
+              axis.title.y = element_text(size = 12, face = "plain"),
+              axis.text = element_text(size = 12, face = "plain"))
+
+
+# Intentions to seek information ---------------------
+cdata |> 
+        select(cstim, rstim, dispcurious, curiosity, frustration, infoseek, dialogue) |> 
+        na.omit() |> 
+        lm(formula = infoseek ~ cstim + rstim + dispcurious) |> 
+        summ() # ns
+
+cdata |> 
+        select(cstim, rstim, dispcurious, curiosity, frustration, infoseek, dialogue) |> 
+        ggplot(aes(x = cstim, y = infoseek)) +
+        scale_y_continuous(name = "Intentions to seek information",
+                           limits = c(1,7),
+                           breaks = seq(1, 7, 1), 
+                           expand = c(0,0)) + 
+        scale_x_discrete(name = "") +
+        theme_apa() +
+        stat_summary(fun = mean, geom = "point") +
+        stat_summary(fun.data = mean_cl_normal, geom = "errorbar", width = 0.2) +
+        theme(title = element_text(size = 12, face = "bold"),
+              axis.title.x = element_text(size = 12, face = "plain"),
+              axis.title.y = element_text(size = 12, face = "plain"),
+              axis.text = element_text(size = 12, face = "plain"))
 
